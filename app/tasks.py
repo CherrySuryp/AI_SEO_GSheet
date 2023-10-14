861bf8a3d4b830e7cf59475aa643c072fde37865
Mark Arkhipychev <hackyourrself@gmail.com>
2023-10-14 20:05:35 +0300

    Celery tasks now ready to use

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 import time
+from typing import Literal
 
 import sentry_sdk
 import requests
@@ -20,43 +21,57 @@ class Worker:
 
     @staticmethod
     @celery.task(soft_time_limit=120, time_limit=180)
-    def parse_wb_item_name(wb_sku: int, row_id: int):
+    def get_parsed_data(
+            mode: Literal["v1", "v1.2", "by_name"],
+            auto_mode: str,
+            wb_sku: int | str,
+            row_id: int
+    ):
         try:
-            item_name = Parser().get_wb_item_name(wb_sku)
-            Worker.gsheet.update_cell(content=item_name, row_id=f"E{row_id}")
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
+            result = requests.post(
+                f"http://91.206.15.62:9000/{wb_sku}",
+                params={"mode": mode, "wb_sku": wb_sku},
+                headers={"x-api-key": Config().PARSER_KEY}
+            )
+            while True:
+                time.sleep(2)
 
-    @staticmethod
-    @celery.task(soft_time_limit=120, time_limit=180)
-    def parse_wb_item_params(wb_sku: int, row_id: int):
-        try:
-            item_params = Parser().get_wb_item_params(wb_sku)
-            item_params = TextUtils().exclude_dim_info(item_params)
-            Worker.gsheet.update_cell(content=str(item_params), row_id=f"F{row_id}")
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
+                task_id = result.json()["task_id"]
+                check = requests.get(
+                    f"http://91.206.15.62:9000/{task_id}/result",
+                    headers={"x-api-key": Config().GPT_KEY}
+                )
+                if result.status_code != 200 or check.status_code != 200:
+                    print(f"exc {row_id}")
+                    Worker.gsheet.update_cell(
+                        "Произошла ошибка сбора данных. "
+                        "Скорее всего все сработает если попробовать еще раз.",
+                        f"J{row_id}"
+                    )
+                    break
+                if check.json()["status"] == "SUCCESS":
+                    result = check.json()["result"]
+                    break
 
-    @staticmethod
-    @celery.task(soft_time_limit=120, time_limit=180)
-    def parse_wb_item_desc(wb_sku: int, row_id: int):
-        try:
-            item_desc = Parser().get_wb_item_desc(wb_sku)
-            Worker.gsheet.update_cell(content=str(item_desc), row_id=f"G{row_id}")
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
+            if mode == "v1":
+                Worker.gsheet.update_cell(result["name"], f"E{row_id}")
+                Worker.gsheet.update_cell(str(result["params"]), f"F{row_id}")
+                Worker.gsheet.update_cell(", ".join(result["keywords"]), f"H{row_id}")
 
-    @staticmethod
-    @celery.task(soft_time_limit=120, time_limit=180)
-    def parse_mpstats_keywords_by_sku(auto_mode: str, wb_sku: int, row_id: int):
-        keywords = None
-        try:
-            keywords = Parser().parse_mpstats_by_sku(wb_sku)
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
+            elif mode == "v1.2":
+                Worker.gsheet.update_cell(result["name"], f"E{row_id}")
+                Worker.gsheet.update_cell(str(result["params"]), f"F{row_id}")
+                Worker.gsheet.update_cell(str(result["desc"]), f"G{row_id}")
+                Worker.gsheet.update_cell(", ".join(result["keywords"]), f"H{row_id}")
 
-        keywords = Worker.text_utils.transform_dict_keys_to_str(keywords) if keywords else None
-        Worker.gsheet.update_cell(row_id=f"H{row_id}", content=keywords)
+            elif mode == "by_name":
+                Worker.gsheet.update_cell(wb_sku, f"E{row_id}")
+                Worker.gsheet.update_cell(", ".join(result["keywords"]), f"H{row_id}")
+
+            print(f"Data written to row {row_id}")
+        except Exception as e:
+            print(e)
+            sentry_sdk.capture_exception(e)
 
         if auto_mode == "Ручной":
             Worker.gsheet.update_status("Завершено", row_id)
@@ -64,41 +79,25 @@ class Worker:
             Worker.gsheet.update_status("Сгенерировать описание", row_id)
 
     @staticmethod
-    @celery.task(soft_time_limit=120, time_limit=180)
-    def parse_mpstats_keywords_by_item_name(auto_mode: str, item_name: str, row_id: int):
-        keywords = None
-        try:
-            keywords = Parser().parse_mpstats_by_name(item_name)
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-
-        keywords = Worker.text_utils.transform_dict_keys_to_str(keywords) if keywords else None
-        Worker.gsheet.update_cell(row_id=f"H{row_id}", content=keywords)
-
-        if auto_mode == "Авто":
-            Worker.gsheet.update_status("Сгенерировать описание", row_id)
-        else:
-            Worker.gsheet.update_status("Завершено", row_id)
-
-    @staticmethod
     @celery.task(soft_time_limit=180, time_limit=240)
     def chatgpt_task(prompt: str, row_id: int) -> None:
         try:
             result = requests.post(
-                f"http://localhost:8000/gpt",
+                f"http://91.206.15.62:8000/gpt",
                 params={"prompt": prompt},
                 headers={"x-api-key": Config().GPT_KEY}
             )
-            print(result.json())
             while True:
+                time.sleep(2)
+
                 task_id = result.json()["task_id"]
                 check = requests.get(
-                    f"http://localhost:8000/{task_id}/result",
+                    f"http://91.206.15.62:8000/{task_id}/result",
                     headers={"x-api-key": Config().GPT_KEY}
                 )
                 if check.status_code == 500:
                     Worker.gsheet.update_cell(
-                        "Произошла ошибка. "
+                        "Произошла ошибка генерации текста. "
                         "Скорее всего все сработает если попробовать еще раз.",
                         f"J{row_id}"
                     )
@@ -110,9 +109,5 @@ class Worker:
                     Worker.gsheet.update_status("Завершено", row_id)
                     Worker.gsheet.update_cell(result, f"J{row_id}")
                     break
-                time.sleep(2)
         except Exception as e:
             sentry_sdk.capture_exception(e)
-
-
-Worker().chatgpt_task.delay(prompt="Привет! Расскажи о себе в двух словах", row_id=2)
